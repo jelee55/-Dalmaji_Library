986513abdb17dd0aa1a238dfb75115d5d444dd5d
euijeongoh <ouj153@naver.com>
2024-01-09 09:53:44 +0900

    notice admin member sql문 작성

--- Dalmaji/src/main/java/com/dalmaji/app/admin/controller/AdminController.java
+++ Dalmaji/src/main/java/com/dalmaji/app/admin/controller/AdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dalmaji.app.admin.service.AdminService;
 import com.dalmaji.app.admin.vo.AdminVo;
+import com.dalmaji.app.member.vo.MemberVo;
 
 import lombok.RequiredArgsConstructor;
 
@@ -19,8 +20,26 @@ public class AdminController {
 	
 	private final AdminService service;
 	
+	//관리자 회원가입 (화면)
+	@GetMapping("adminjoin")
+	public String join() {
+		return "admin/adminjoin";
+	}
+	
+	//회원가입
+	@PostMapping("adminjoin")
+	public String join(AdminVo vo) throws Exception {
+		int result = service.join(vo);
+		
+		if(result != 1) {
+			throw new Exception();
+		}
+		
+		return "redirect:/home";	//회원가입 성공시 홈페이지로 redirect
+	}
+	
 	//로그인
-	@PostMapping("login")
+	@PostMapping("adminlogin")
 	public String login(AdminVo vo, HttpSession session) throws Exception {
 		
 		AdminVo loginAdmin = service.login(vo);
@@ -36,7 +55,7 @@ public class AdminController {
 	}
 	
 	//로그아웃
-	@GetMapping("logout")
+	@GetMapping("adminlogout")
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "redirect:/home";
