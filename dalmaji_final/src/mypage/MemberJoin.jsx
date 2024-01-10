a82fffab1236b1d1c88305a65c2f17b09d85c7cf
euijeongoh <ouj153@naver.com>
2024-01-10 09:50:05 +0900

    Mypage join, login React 수정 중

--- dalmaji_final/src/mypage/MemberJoin.jsx
+++ dalmaji_final/src/mypage/MemberJoin.jsx
@@ -1,33 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
-const StyledMypageJoinDiv = styled.div`
+const StyledMemberJoinDiv = styled.div`
     width: 100%;
     height: 100%;
     display: grid;
     grid-template-rows: 1fr 1.5fr;
     place-items: center center;
     background-color: red;
-    `;
-
+`;
 
 const MemberJoin = () => {
+    const navigate = useNavigate();
+    const [vo] = useState({
+        id: "",
+        pwd: "",
+        name: ""
+    });
+
+    const isFetching = false; // isFetching 변수를 선언
+
+    const handleJoinSubmit = (event) => {
+        event.preventDefault();
+
+        // 작업을 해도 되나 안해도 되나 검사하는 작업
+        if (isFetching) {
+            return;
+        }
+
+    // 작업 시작
+    isFetching = true;
+    
+
+    fetch("http://127.0.0.1:8888/app/rest/member/join" , {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(vo)
+    })
+    .then( resp => {
+        if(!resp.ok){
+            throw new Error("회원가입 fetch 실패..");
+        }
+        return resp.json();
+    } )
+    .then( data => {
+        if( data.msg === "good" ){
+            alert("회원가입 성공 !");
+            navigate("/");
+        }else{
+            alert("회원가입 실패 ...");
+            navigate("/failpage~~~");
+        }
+        
+    } )
+    .catch( (e) => {
+        console.log(e);
+        alert("회원가입 실패");
+    } )
+    .finally( () => {
+        isFetching = false;
+    } )
+    ;
+}
+
+
     return (
-        <StyledMypageJoinDiv>
-            <form onSubmit={ handleJoinSubmit }>
+        <StyledMemberJoinDiv>
+            <form onSubmit={handleJoinSubmit}>
                 <table>
                     <tbody>
                         <tr>
                             <td>아이디</td>
-                            <td><input type="text" name='id' onChange={handleInputChange} /></td>
+                            <td><input type="text" name='id'/></td>
                         </tr>
                         <tr>
                             <td>비밀번호</td>
-                            <td><input type="password" name='pwd' onChange={handleInputChange} /></td>
+                            <td><input type="password" name='pwd'/></td>
                         </tr>
                         <tr>
                             <td>이름</td>
-                            <td><input type="text" name='name' onChange={handleInputChange} /></td>
+                            <td><input type="text" name='name'/></td>
                         </tr>
                         <tr>
                             <td><input type='reset' /></td>
@@ -36,7 +91,7 @@ const MemberJoin = () => {
                     </tbody>
                 </table>
             </form>
-        </StyledMypageJoinDiv>
+        </StyledMemberJoinDiv>
     );
 };
 
--- dalmaji_final/src/mypage/MypageJoin.jsx
+++ dalmaji_final/src/mypage/MypageJoin.jsx
@@ -1,10 +1,98 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledMypageJoinDiv = styled.div`
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template-rows: 1fr 1.5fr;
+    place-items: center center;
+    background-color: red;
+    `;
 
 const MypageJoin = () => {
+
+    const navigate = useNavigate();
+
+    let isFetching = false;
+    const [vo] =  useState({
+        id : "",
+        pwd : "",
+        nick: ""
+    });
+
+const handleJoinSubmit = (event) => {
+    event.preventDefault();
+
+    //작업을 해도되나 안해도되나 검사하는 작업
+    if(isFetching){
+        return;
+    }
+
+    //작업시작
+    isFetching = true;
+    
+
+    fetch("http://127.0.0.1:8888/app/rest/member/join" , {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(vo)
+    })
+    .then( resp => {
+        if(!resp.ok){
+            throw new Error("회원가입 fetch 실패..");
+        }
+        return resp.json();
+    } )
+    .then( data => {
+        if( data.msg === "good" ){
+            alert("회원가입 성공 !");
+            navigate("/");
+        }else{
+            alert("회원가입 실패 ...");
+            navigate("/failpage~~~");
+        }
+        
+    } )
+    .catch( (e) => {
+        console.log(e);
+        alert("회원가입 실패");
+    } )
+    .finally( () => {
+        isFetching = false;
+    } )
+    ;
+}
+
+
     return (
-        <div>
-            <h1>joinnnnnnnnnnnnnn</h1>
-        </div>
+        <StyledMypageJoinDiv>
+            <form onSubmit={ handleJoinSubmit }>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>아이디</td>
+                            <td><input type="text" name='id'/></td>
+                        </tr>
+                        <tr>
+                            <td>비밀번호</td>
+                            <td><input type="password" name='pwd'/></td>
+                        </tr>
+                        <tr>
+                            <td>이름</td>
+                            <td><input type="text" name='name'/></td>
+                        </tr>
+                        <tr>
+                            <td><input type='reset' /></td>
+                            <td><input type="submit" value="회원가입"/></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </StyledMypageJoinDiv>
     );
 };
 
