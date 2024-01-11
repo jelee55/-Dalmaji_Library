35953e72d72e4e092687b0e248f563e8f9515aa8
JINILEEE <douob55@gmail.com>
2024-01-11 17:39:55 +0900

    20240111_MJ [페이징 처리중..... & 오라클 전자지갑 사용설정]

--- dalmaji_final/src/admin/AdminMain.jsx
+++ dalmaji_final/src/admin/AdminMain.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import SearchAdminDelete from '../search/SearchAdminDelete';
-import AdminMyPage from '../mypage/AdminMyPage';
+import AdminMyPage from './AdminMyPage';
 import { Route, Routes } from 'react-router-dom';
 import SearchAdminEdit from '../search/SearchAdminEdit';
 
 const AdminMain = () => {
     return (
         <Routes>
-            <Route path='/myoage/*' element={<AdminMyPage />}></Route>
+            <Route path='/mypage/*' element={<AdminMyPage />}></Route>
             <Route path='/edit/*' element={<SearchAdminEdit />}></Route>
             <Route path='/delete/*' element={<SearchAdminDelete />}></Route>
         </Routes>
--- dalmaji_final/src/component/Pagination.jsx
+++ dalmaji_final/src/component/Pagination.jsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledPagingDiv = styled.div`
+    margin-top: 12px;
+    text-align: center;
+`;
+
+const StyledPageBtn = styled.button`
+    height: 34px;
+    border-radius: 7px;
+    padding: 0 12px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.5;
+    cursor: pointer;
+    box-sizing: border-box;
+    position: relative;
+`;
 
 const Pagination = () => {
     return (
