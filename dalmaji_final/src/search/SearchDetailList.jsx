5c224f5483aa883e8c112c2f12e48b0576c74244
JELEE\User <jelee55>
2024-01-16 16:51:49 +0900

    BOOKLIST페이징처리중~~

--- dalmaji_final/src/search/SearchDetailList.jsx
+++ dalmaji_final/src/search/SearchDetailList.jsx
@@ -3,93 +3,172 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const StyledDetailListDiv = styled.div`
-    width: 100%;
-    height: 100%;
-    display: grid;
-    /* grid-template-rows: 20% 60% 20%; */
-    place-items: center center;
-    background-color: red;
-& > div {
-    width: 100%;
-    height: 100%;
+  width: 100%;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 2fr 1fr 5fr;
+  place-items: center center;
+  background-color: #8c00ff;
+  padding: 0%;
+  
+  & > .header1 {
+      width: 80%;
+      height: 20%;
+      margin: auto;
+      background-color: #9af097;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      border-bottom: 3px solid black;
+      padding: 0%;
+
+    h1 {
+        width: 100%;
+        margin: 0;
+        padding: 0;
+    }
+  
 }
-& > .header {
+& > .header2 {
     width: 80%;
-    height: 20%;
-    padding: 0%;
-    margin: auto;
-    background-color: blue;
-    display: grid;
-    place-items: center center;
-    
-}
-& > .header h1 {
-            border-bottom: 3px solid black;
-            width: 100%;
+    margin: none;
+    display: flex;
+    /* justify-content: ; */
+    margin-top: 10px;  /* .header2 위 간격 조절 */
+    background-color: yellow;
+    .btn {
+      display: flex;
+      justify-content: space-between;
+      /* color: #8c00ff; */
+    }
+  }
 
-        }
-& > .btn button {
-    /* display: flex;
-    justify-content: space-evenly;
-    color: yellow; */
-}     
 & > form {
-        width: 60%;
-        height: 80%;
-        /* border: 2px solid black; */
-        /* background-color: beige; */
+    width: 40%;  /* 화면의 45%로 설정 */
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-}
+    & > div {
+        width: 100%;
+        margin-top: 10px;
+
+        .title,
+        .author,
+        .company {
+            margin-bottom: 5px;
+        }
+
+        input {
+            width: 100%;
+            
+        }
+    }
+    .search-buttons {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        margin-top: 10px;
+
+        input[type="submit"],
+        input[type="reset"] {
+            width: 48%;
+        }
+    }
+  }
 `;
 
-const SearchDetailList = () => {    
-   
+const SearchDetailList = () => {
+  const [bookVoList, setBookVoList] = useState([]);
+  const [searchValues, setSearchValues] = useState({
+    title: '',
+    author: '',
+    company: '',
+  });
 
-    //fetch 이용해 데이터 준비
-    const [bookVoList, setBookVoList] = useState([]);
-    const loadBookVoList = () => {
-        fetch("http://127.0.0.1:8888/app/search/detaillist")
-            .then(resp => resp.json())
-            .then((data) => {
-                console.log(data);
-                setBookVoList(data);})
-            ;
-    }
+  const loadBookVoList = () => {
+    fetch('http://127.0.0.1:8888/app/search/detaillist')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setBookVoList(data);
+      });
+  };
 
-    useEffect(() => {
-        console.log("useEffect 호출됨");
-        loadBookVoList();
-    }, []);
+  useEffect(() => {
+    loadBookVoList();
+  }, []);
 
+  const handleInputChange = (e) => {
+    setSearchValues({
+      ...searchValues,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    return (
-        
-        <StyledDetailListDiv>
-            <div className='header'>
-            <div><h1>도서검색</h1></div>
-            <div className='btn'><Link to="/search/list"><button>전체보기</button></Link></div> 
-            <div className='btn'><Link to="/search/detaillist"><button>검색</button></Link></div> 
+  const handleSearch = (e) => {
+    e.preventDefault();
+    // 실제 서버 API를 기반으로 검색 기능을 구현하세요
+    console.log('검색:', searchValues);
+  };
+
+  return (
+    <StyledDetailListDiv>
+      <div className="header1">
+        <div >
+          <h1>도서검색</h1>
         </div>
-        <form >
-            <div>
-                <div className='title'>제목</div>
-                <div><input type="text" name='title' /></div>
-                <div className='author'>작성자</div>
-                <div><input type="text" name='author' /></div>
-                <div className='company'>출판사</div>
-                <div><input type="text" name='company' /></div>
-                <div>
-                    <input type='submit' value="검색" title="검색" className='searhB'/>
-                    <input type='reset' value="다시쓰기"title="다시쓰기"/>
-                </div>
-                
-           </div>
-        </form>
-
-
-
-      
+      </div>
+      <div className='header2'>
+        <div className="btn">
+          <Link to="/search/list">
+            <button>전체보기</button>
+          </Link>
+        </div>
+        <div className="btn">
+          <Link to="/search/detaillist">
+            <button>검색</button>
+          </Link>
+        </div>
+      </div>
+      <form onSubmit={handleSearch}>
+        <div>
+          <div className="title">제목</div>
+          <div>
+            <input
+              type="text"
+              name="title"
+              value={searchValues.title}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="author">작성자</div>
+          <div>
+            <input
+              type="text"
+              name="author"
+              value={searchValues.author}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="company">출판사</div>
+          <div>
+            <input
+              type="text"
+              name="company"
+              value={searchValues.company}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <input type="submit" value="검색" title="검색" className="searchB" />
+            <input type="reset" value="다시쓰기" title="다시쓰기" />
+          </div>
+        </div>
+      </form>
     </StyledDetailListDiv>
-    );
+  );
 };
+
 export default SearchDetailList;
