b026ba9926f25cf4879a5003ac6c207dd0e92fdf
JELEE\User <jelee55>
2024-01-17 14:32:56 +0900

    jelee[페이징 끝 & 검색 화면 구현중~~~ ]

--- dalmaji_final/src/search/SearchList.jsx
+++ dalmaji_final/src/search/SearchList.jsx
@@ -79,24 +79,24 @@ const SearchList = () => {
     const [currentPage, setCurrentPage] = useState(1);  // 현재 페이지 상태 추가
     const [totalPages, setTotalPages] = useState(1);    // 전체 페이지 수 상태 추가
     
-    const loadBookVoList = () => {
-        fetch("http://127.0.0.1:8888/app/search/list")
-            .then(resp => resp.json())
-            .then((data) => {
-                console.log(data);
-                setBookVoList(data);})
-            ;
-    }
+    // const loadBookVoList = () => {
+    //     fetch("http://127.0.0.1:8888/app/search/list")
+    //         .then(resp => resp.json())
+    //         .then((data) => {
+    //             console.log(data);
+    //             setBookVoList(data);})
+    //         ;
+    // }
 
-    useEffect(() => {
-        console.log("useEffect 호출됨");
-        loadBookVoList();
-    }, []);
+    // useEffect(() => {
+    //     console.log("useEffect 호출됨");
+    //     loadBookVoList();
+    // }, []);
 
 
 
         //fetch 이용해 데이터 준비 (페이지 처리)
-        const loadBookVoListTwo = (page) => {
+        const loadBookVoList = (page) => {
         
             // URL 문자열 안에 변수를 넣을 때는 백틱(``)을 사용하고, 변수는 ${}로 감싸줌
             fetch(`http://127.0.0.1:8888/app/search/list?currentPage=${page}`, {
@@ -122,7 +122,7 @@ const SearchList = () => {
     }
     
     useEffect( () => {
-        loadBookVoListTwo(currentPage); //현재 페이지의 목록 불러오기
+        loadBookVoList(currentPage); //현재 페이지의 목록 불러오기
     }, [currentPage] );
     
     useEffect( () => {
