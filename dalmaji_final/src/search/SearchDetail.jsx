92dfa93db820046e3754a6d6c999acd27c868d5f
JINILEEE <douob55@gmail.com>
2024-01-18 12:09:06 +0900

    20240118_MJ [모달창 구현 진행중...]

--- dalmaji_final/src/search/SearchDetail.jsx
+++ dalmaji_final/src/search/SearchDetail.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import Modal from "react-modal";
 
 const StyledSearchDetailDiv = styled.div`
     width: 100%;
@@ -14,7 +18,7 @@ const StyledDetailContentDiv = styled.div`
     width: 100%;
     height: 100%;
     display: grid;
-    grid-template-rows: 1fr 5fr 3fr 2fr;
+    grid-template-rows: 1fr 5fr 3fr 1fr;
     & > div:first-child > h1 {
             margin-top: 50px;
             border-bottom: 3px solid black;
@@ -48,6 +52,11 @@ const StyledDetailContentDiv = styled.div`
 const StyledTableDiv = styled.div`
     width: 100%;
     height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
     & > div:first-child {
         width: 100%;
         height: 50px;
@@ -70,6 +79,69 @@ const StyledTableDiv = styled.div`
         & > tbody > tr {
             width: 100%;
             height: 30px;
+        }
+    }
+    & > button {
+        width: 80px;
+        height: 35px;
+        font-size: 18px;
+        margin-top: 30px;
+        background-color: #275FBC;
+        border: none;
+        border-radius: 7px;
+        color: white;
+        cursor: pointer;
+    }
+    & > button:hover{
+        filter: brightness(130%);
+    }
+`;
+
+const StyledModalDiv = styled.div`
+    z-index: 100;
+    width: 500px;
+    height: 400px;
+    background-color: white;
+    box-shadow: 3px 3px 6px 2px rgba(169, 169, 169, 0.5);
+    border-radius: 20px;
+    position: fixed;
+    display: grid;
+    grid-template-rows: 1.5fr 1fr 1fr 1fr;
+    place-items: center center;
+    top: 25vh;
+    left: 35vw;
+    & > div:first-child {
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        background-color: #EFEFF1;
+        padding-top: 40px;
+        border-top-left-radius: 20px;
+        border-top-right-radius: 20px;
+    }
+    & > div:nth-child(2) {
+        font-size: 25px;
+    }
+    & > input {
+        width: 200px;
+        height: 40px;
+        font-size: 25px;
+        padding-left: 7px;
+    }
+    & > div:nth-child(4) {
+        display: flex;
+        gap: 10px;
+        & > button {
+            width: 50px;
+            height: 30px;
+            border: none;
+            border-radius: 7px;
+            background-color: #666666;
+            color: white;
+            cursor: pointer;
+        }
+        & > button:hover {
+            background-color: #E72900;
         }
     }
 `;
@@ -103,9 +175,20 @@ const SearchDetail = () => {
         loadBookDetailVo();
     }, [selectedBookNo.bookNo] )
 
+    // 모달창을 위한 준비
+    const [modal, setModal] = useState(false);
+
+    const openModal = () => {
+        setModal(true);
+    };
+
+    const closeModal = () => {
+        setModal(false);
+    };
+    
     return (
         <StyledSearchDetailDiv>
-            <div>1</div>
+            <div></div>
             <StyledDetailContentDiv>
                 <div><h1>상세정보</h1></div>
                 <div>
@@ -139,11 +222,25 @@ const SearchDetail = () => {
                             </tr>
                         </tbody>
                     </table>
-                    <button>대출</button>
+                    <button onClick={ () => { setModal(!modal) } }><FontAwesomeIcon icon={faBook} /> 대출</button>
+                    {modal === true 
+                        ? 
+                        <StyledModalDiv>
+                            <div><h1>알림</h1></div>
+                            <div> <FontAwesomeIcon icon={faLock} /> 대출 비밀번호를 입력해주세요.</div>
+                            <input type="password" placeholder='password'/>
+                            <div>
+                                <button>완료</button>
+                                <button onClick={ () => {setModal(modal)} }>취소</button>
+                            </div>
+                        </StyledModalDiv> 
+                        : 
+                        null
+                    }
                 </StyledTableDiv>
                 <div>4</div>
             </StyledDetailContentDiv>
-            <div>3</div>
+            <div></div>
         </StyledSearchDetailDiv>
     );
 };
